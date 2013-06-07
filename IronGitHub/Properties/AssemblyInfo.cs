54fc153405a6e3572d372a653832dcdecfbb5f77
Tim Erickson <tim@in2bits.org>
2013-06-06 21:54:01 -0700

    updated AssemblyInfo.cs and added NuGet spec for publishing to NuGet

--- IronGitHub/Properties/AssemblyInfo.cs
+++ IronGitHub/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IronGitHub")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("IronGitHub - in2bits")]
+[assembly: AssemblyDescription("GitHub API v3 client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("in2bits")]
 [assembly: AssemblyProduct("IronGitHub")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
