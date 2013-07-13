293a9e45311db7bba4a62dae9c38554d33253348
Ken Warner <kjwarner@gmail.com>
2013-07-13 12:57:57 -0400

    Add OrganizationApi with GetOrganization and GetRepositories

---
+++ IronGitHub/Apis/OrganizationsApi.cs
@@ -0,0 +1,52 @@
+﻿using IronGitHub.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronGitHub.Apis
+{
+    /// <summary>
+    /// Organizations API
+    /// 
+    /// http://developer.github.com/v3/orgs/
+    /// </summary>
+    public class OrganizationsApi : GitHubApiBase
+    {
+        public OrganizationsApi(GitHubApiContext context) 
+            : base(context)
+        {
+        }
+
+        /// <summary>
+        /// Get a single Organization
+        /// 
+        /// /orgs/:org
+        /// </summary>
+        /// <returns>The authenticated User</returns>
+        async public Task<Organization> GetOrganization(string organization)
+        {
+            var request = CreateRequest(string.Format("/orgs/{0}", organization));
+            var response = await Complete<Organization>(request);
+            return response.Result;
+        }
+
+        /// <summary>
+        /// Gets the repositories belonging to an Organization
+        /// 
+        /// /orgs/:org/repos
+        /// </summary>
+        /// <returns>The repositories belonging to an Organization</returns>
+        async public Task<IEnumerable<Repository>> GetRepositories(string organization, RepositoryTypes? type = null)
+        {
+            var request = CreateRequest(string.Format("/orgs/{0}/repos", organization),
+                new Dictionary<string, string>
+                    {
+                        {"type", type.ToParameterValue()}
+                    });
+            var response = await Complete<IEnumerable<Repository>>(request);
+            return response.Result;
+        }
+    }
+}
--- IronGitHub/Entities/Organization.cs
+++ IronGitHub/Entities/Organization.cs
@@ -6,5 +6,35 @@ namespace IronGitHub.Entities
     [DataContract]
     public class Organization : Entity
     {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "company")]
+        public string Company { get; set; }
+
+        [DataMember(Name = "blog")]
+        public string Blog { get; set; }
+
+        [DataMember(Name = "location")]
+        public string Location { get; set; }
+
+        [DataMember(Name = "email")]
+        public string Email { get; set; }
+
+        [DataMember(Name = "public_repos")]
+        public int PublicRepositoryCount { get; set; }
+
+        [DataMember(Name = "public_gists")]
+        public int PublicGistCount { get; set; }
+
+        [DataMember(Name = "followers")]
+        public int FollowersCount { get; set; }
+
+        [DataMember(Name = "following")]
+        public int FollowingCount { get; set; }
+
+        [DataMember(Name = "created_at")]
+        public DateTime CreatedAt { get; set; }
+
     }
 }
