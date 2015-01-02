ed1ea0414dc4f3408285e164e4d6b04deb520fab
Erik Zettersten <Erik@Zettersten.com>
2015-01-01 23:13:47 -0500

    added webhooks

---
+++ IronGitHub.Tests/WebHooks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IronGitHub.Tests.Helpers;
+using FluentAssertions;
+
+
+namespace IronGitHub.Tests
+{
+    [TestClass]
+    public class WebHooks : AuthenticationBoilerPlate
+    {
+        [TestMethod]
+        public void Should_Setup_GitHub_Api()
+        {
+            this.CreateGitHubApi();
+            this.Api.Should().NotBeNull();
+        }
+    }
+}
